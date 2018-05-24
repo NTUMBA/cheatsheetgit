174829de4fa3c15c344bab33dc0b2d5a31aa603c
NTUMBA <cityzenmboma@yahoo.fr>
2018-05-25 01:08:50 +0200

    Adding files and fixing links and pictures

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ function main() {
   $('.logo').fadeIn(3000);
   $('.cognito').hide();
   $('.cognito').fadeIn(15000);
+  $('.detail').hide();
+$('.definition').on('click', function() {
+$('.detail').toggle();
+});
   $('.memento').hide();
 $('.conseil').on('click', function() {
 $('.memento').toggle();
