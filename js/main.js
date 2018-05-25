05c9196f56af30db39746ecf3a3bf20ed961540b
NTUMBA <cityzenmboma@yahoo.fr>
2018-05-25 03:20:49 +0200

    Deleting file

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ $(document).ready(main);
 
 function main() {
   $('.logo').hide();
-  $('.logo').fadeIn(3000);
+  $('.logo').fadeIn(2000);
   $('.cognito').hide();
-  $('.cognito').fadeIn(15000);
+  $('.cognito').fadeIn(10000);
   $('.detail').hide();
 $('.definition').on('click', function() {
 $('.detail').toggle();
